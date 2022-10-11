2ee4bfd5acb88a76c64ab4f3f18796c02e642e5b
Jean-LC <jeanettelc@outlook.com>
2022-10-11 17:26:09 -0500

    connects validation functions to main in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const readFile = require ('./read-file.js');
-
+const path = require ('path');
 let receivedPath = process.argv[2];
 
 readFile(receivedPath);
+console.log(path.extname(receivedPath));
