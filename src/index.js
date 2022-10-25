c305e4680df6df434f8dfa0b784e53d0b03aa251
Jean-LC <jeanettelc@outlook.com>
2022-10-25 10:54:19 -0500

    returns changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const readFile = require ('./read-file.js');
+const readFile = require('./read-file.js');
 let receivedPath = process.argv[2];
 let firstCommand = process.argv[3];
 let secondCommand = process.argv[4];
 
-
 readFile(receivedPath, firstCommand, secondCommand)
-.then ((message) => console.log(message));
+    .then((message) => console.log(message));
