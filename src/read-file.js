89cca611faf2ce54108830d604b99d0efac48cb1
Jean-LC <jeanettelc@outlook.com>
2022-10-25 10:53:40 -0500

    leaves a unique promise in read-file.js

--- src/read-file.js
+++ src/read-file.js
@@ -1,14 +1,11 @@
-const colors = require('colors');
 const fn = require('./fn.js')
-
-
 
 module.exports = (receivedPath, firstCommand, secondCommand) => {
     return new Promise((resolve) => {
         if (secondCommand) {
             getListOfLinks(receivedPath)
-                .then((links) => validateLinks(links))
-                .then((data) => resolve(statsLinks(data, secondCommand)))
+                .then((links) => fn.validateLinks(links))
+                .then((data) => resolve(fn.statsLinks(data, secondCommand)))
                 .catch((error) => resolve(error))
         } else if (firstCommand === undefined) {
             getListOfLinks(receivedPath)
@@ -16,12 +13,12 @@ module.exports = (receivedPath, firstCommand, secondCommand) => {
                 .catch((error) => resolve(error))
         } else if (firstCommand === 'validate') {
             getListOfLinks(receivedPath)
-                .then((links) => validateLinks(links))
+                .then((links) => fn.validateLinks(links))
                 .then((data) => resolve(data))
                 .catch((error) => resolve(error))
         } else if (firstCommand === 'stats') {
             getListOfLinks(receivedPath)
-                .then((links) => resolve(statsLinks(links)))
+                .then((links) => resolve(fn.statsLinks(links)))
                 .catch((error) => resolve(error))
         }
     })
@@ -37,55 +34,3 @@ const getListOfLinks = (path) => {
             .catch((error) => reject(error))
     })
 }
-
-// returns list of links with validation
-const validateLinks = (arr) => {
-    return new Promise((resolve) => {
-        let arrFn = []
-        for (let i = 0; i < arr.length; i++) {
-            arrFn.push(fn.axiosValidation(arr[i]))
-        }
-        console.log(arrFn)
-        Promise.all(arrFn)
-            .then((values) => resolve(values))
-    })
-}
-
-// returns stats of links
-const statsLinks = (arr, command) => {
-    let countLinks = []
-    let countFiles = new Set()
-    let countValidatedTrue = []
-    let countValidatedFalse = []
-    let finalArray = []
-
-    if (command) {
-        for (let i = 0; i < arr.length; i++) {
-            countLinks.push(arr[i].href)
-            countFiles.add(arr[i].file)
-            if (arr[i].status === 'FAIL: 404') {
-                countValidatedFalse.push(arr[i].status)
-            } else {
-                countValidatedTrue.push(arr[i].status)
-            }
-        }
-        finalArray = [
-            'files: ' + countFiles.size,
-            'links: ' + countLinks.length,
-            'OK links: ' + countValidatedTrue.length,
-            'Failed links: ' + countValidatedFalse.length
-        ]
-
-    } else {
-        for (let i = 0; i < arr.length; i++) {
-            countLinks.push(arr[i].href)
-            countFiles.add(arr[i].file)
-        }
-        finalArray = [
-            'files: ' + countFiles.size,
-            'links: ' + countLinks.length,
-        ]
-    }
-
-    return (finalArray)
-} 
