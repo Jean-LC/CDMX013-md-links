79b303278d9d6c793fcbc195996850a6ec8964c8
Jean-LC <jeanettelc@outlook.com>
2022-10-25 12:31:33 -0500

    tries an exercise with node modules in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const readFile = require('./read-file.js');
-let receivedPath = process.argv[2];
-let firstCommand = process.argv[3];
-let secondCommand = process.argv[4];
+// let receivedPath = process.argv[2];
+// let firstCommand = process.argv[3];
+// let secondCommand = process.argv[4];
 
-readFile(receivedPath, firstCommand, secondCommand)
-    .then((message) => console.log(message));
+module.exports = (args) => { 
+    console.log(args)
+    /*readFile(path, firstComm, secondComm)
+    .then((message) => console.log(message)); */
+}
