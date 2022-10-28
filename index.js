84a86ac39022270a4a1ac9d7db0532ab0ef9542a
Jean-LC <jeanettelc@outlook.com>
2022-10-28 11:57:22 -0500

    changes index.js to console.log with colors and receive args

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-module.exports = () => {
-  // ...
+const readFile = require("./read-file.js");
+const colors = require('colors')
+
+
+module.exports = (arg) => {
+    const pathReceived = arg[2]
+    const validate = arg.includes('--validate')
+    const stats = arg.includes('--stats')
+
+    console.log(' Welcome to MdLinks! '.bgYellow.black)
+    console.log(('Reading path: ' + pathReceived).yellow + '\n')
+    readFile(pathReceived, validate, stats)
+        .then((array) => array.forEach((obj) => {
+            if (obj.status) {
+                console.log(
+                    'status: '.cyan + obj.status,
+                    '\n',
+                    'href: '.cyan + obj.href,
+                    '\n',
+                    'text: '.cyan + obj.text,
+                    '\n',
+                    'file: '.cyan + obj.file,
+                    '\n'
+                )
+            } else if (obj.href) {
+                console.log(
+                    'href: '.cyan + obj.href,
+                    '\n',
+                    'text: '.cyan + obj.text,
+                    '\n',
+                    'file: '.cyan + obj.file,
+                    '\n'
+                )
+            } else {
+                console.log(obj.cyan)
+            }
+        }))
+
+        .catch(err => console.log(err.red))
 };
