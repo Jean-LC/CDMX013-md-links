cc3ae6a5362fdd6a4b7249790474dd2f46de0a6f
Jean-LC <jeanettelc@outlook.com>
2022-10-28 11:57:48 -0500

    moves files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const readFile = require('./read-file.js');
-
-module.exports = (path, firstComm, secondComm) => {
-    readFile(path, firstComm, secondComm)
-    .then((message) => console.log(message))
-    .catch((message) => console.log(message))
-}
