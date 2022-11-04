e0fba4101ef1733cdcc31d98d663bde47e30b801
Jean-LC <jeanettelc@outlook.com>
2022-11-04 09:59:24 -0600

    changes error from axios to receive code in fn.js

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,9 +1,9 @@
-module.exports = (urlReceived) => {
+module.exports = jest.fn((urlReceived) => {
     return new Promise((resolve, reject) => {
         if(urlReceived.url === 'https://nodejs.or'){
-            reject()
+            reject({ response: {status: '404'}})
         } else {
             resolve({ statusText: 'OK', status: '200'})
         }
     })
-}
+})
